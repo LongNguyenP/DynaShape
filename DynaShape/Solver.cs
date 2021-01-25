921024624d1655531df79ff2a3fcd0eabcb3f389
Long Nguyen <longnguyen.connect@gmail.com>
2021-01-25 23:51:11 +0700

    .

--- DynaShape/Solver.cs
+++ DynaShape/Solver.cs
@@ -45,13 +45,16 @@ namespace DynaShape
             TimeCreated = DateTime.Now;
             CurrentIteration = 0;
 #if CLI == false
-            if (DynaShapeViewExtension.ViewModel == null) throw new Exception("Oh no, DynaShape could not get access to the Helix ViewModel. Sad!");
+
             Display = new DynaShapeDisplay(this);
-            DynaShapeViewExtension.ViewModel.ViewMouseDown += ViewportMouseDownHandler;
-            DynaShapeViewExtension.ViewModel.ViewMouseUp += ViewportMouseUpHandler;
-            DynaShapeViewExtension.ViewModel.ViewMouseMove += ViewportMouseMoveHandler;
-            DynaShapeViewExtension.ViewModel.ViewCameraChanged += ViewportCameraChangedHandler;
-            DynaShapeViewExtension.ViewModel.CanNavigateBackgroundPropertyChanged += ViewportCanNavigateBackgroundPropertyChangedHandler;
+            if (DynaShapeViewExtension.ViewModel != null)
+            {
+                DynaShapeViewExtension.ViewModel.ViewMouseDown += ViewportMouseDownHandler;
+                DynaShapeViewExtension.ViewModel.ViewMouseUp += ViewportMouseUpHandler;
+                DynaShapeViewExtension.ViewModel.ViewMouseMove += ViewportMouseMoveHandler;
+                DynaShapeViewExtension.ViewModel.ViewCameraChanged += ViewportCameraChangedHandler;
+                DynaShapeViewExtension.ViewModel.CanNavigateBackgroundPropertyChanged += ViewportCanNavigateBackgroundPropertyChangedHandler;
+            }
 #endif
         }
 
