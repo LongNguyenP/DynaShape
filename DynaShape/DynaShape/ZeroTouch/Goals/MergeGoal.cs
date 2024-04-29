7cccc34792fc579ee3b82f021613db8060497d68
John <sixtysecondrevit@gmail.com>
2024-04-29 15:31:33 -0600

    MergeGoal

--- DynaShape/DynaShape/ZeroTouch/Goals/MergeGoal.cs
+++ DynaShape/DynaShape/ZeroTouch/Goals/MergeGoal.cs
@@ -12,13 +12,13 @@ namespace DynaShape.ZeroTouch.Goals
         private MergeGoal(){}
 
         /// <summary>
-        /// Pull a set of nodes to their center of mass.
+        /// Creates a MergeGoal to pull a set of nodes to their center of a given mass.
         /// This is useful to force the nodes to have coincident positions.
         /// By default the weight value is set very high to ensure that the nodes really merge together at one position.
         /// </summary>
-        /// <param name="startPositions"></param>
-        /// <param name="weight"></param>
-        /// <returns></returns>
+        /// <param name="startPositions">The starting positions of the nodes.</param>
+        /// <param name="weight">The goal's weight/impact on the solver.</param>
+        /// <returns name="MergeGoal"></returns>
         [NodeCategory("Create")]
         public static DynaShape.Goals.MergeGoal Create(
             List<Point> startPositions,
@@ -29,18 +29,18 @@ namespace DynaShape.ZeroTouch.Goals
 
 
         /// <summary>
-        /// Adjust the goal's parameters while the solver is running.
+        /// Modifies the MergeGoal's parameters while the solver is running.
         /// </summary>
-        /// <param name="goal"></param>
-        /// <param name="weight"></param>
+        /// <param name="mergeGoal">The MergeGoal to modify.</param>
+        /// <param name="weight">An optional new weight for the MergeGoal.</param>
         /// <returns></returns>
         [NodeCategory("Actions")]
         public static DynaShape.Goals.MergeGoal Change(
-            DynaShape.Goals.MergeGoal goal,
+            DynaShape.Goals.MergeGoal mergeGoal,
             [DefaultArgument("-1.0")] float weight)
         {
-            if (weight >= 0.0) goal.Weight = weight;
-            return goal;
+            if (weight >= 0.0) mergeGoal.Weight = weight;
+            return mergeGoal;
         }
     }
 }
