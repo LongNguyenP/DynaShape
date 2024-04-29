5855c28d4f1b2390cccec246aec3fe33e3692bac
John <sixtysecondrevit@gmail.com>
2024-04-29 15:52:56 -0600

    ShapeMatchingGoal

--- DynaShape/DynaShape/ZeroTouch/Goals/ShapeMatchingGoal.cs
+++ DynaShape/DynaShape/ZeroTouch/Goals/ShapeMatchingGoal.cs
@@ -12,14 +12,14 @@ namespace DynaShape.ZeroTouch.Goals
         private ShapeMatchingGoal(){}
 
         /// <summary>
-        /// Move a set of nodes to positions that resemble a target shape.
-        /// The target shape is defined by a sequence of points, in the same order as how the nodes are specified.
+        /// Creates a ShapeMatchingGoal to move a set of nodes to positions that resemble a given target shape.
+        /// The target shape is defined by a sequence of points, in the desired order.
         /// </summary>
-        /// <param name="startPositions"></param>
-        /// <param name="targetShapePoints"></param>
-        /// <param name="allowScaling"></param>
-        /// <param name="weight"></param>
-        /// <returns></returns>
+        /// <param name="startPositions">The start positions of the nodes.</param>
+        /// <param name="targetShapePoints">The points to move the nodes to.</param>
+        /// <param name="allowScaling">Allow scaling of the points to maintain the shape.</param>
+        /// <param name="weight">The goal's weight/impact on the solver.</param>
+        /// <returns name="ShapeMatchingGoal"></returns>
         [NodeCategory("Create")]
         public static DynaShape.Goals.ShapeMatchingGoal Create(
             List<Point> startPositions,
@@ -35,24 +35,24 @@ namespace DynaShape.ZeroTouch.Goals
 
 
         /// <summary>
-        /// Adjust the goal's parameters while the solver is running.
+        /// Modifies the ShapeMatchingGoal's parameters while the solver is running.
         /// </summary>
-        /// <param name="goal"></param>
-        /// <param name="targetShapePoints"></param>
-        /// <param name="allowScaling"></param>
-        /// <param name="weight"></param>
-        /// <returns></returns>
+        /// <param name="shapeMatchingGoal">The ShapeMatchingGoal to modify.</param>
+        /// <param name="targetShapePoints">An optional new list of points to move the nodes to.</param>
+        /// <param name="allowScaling">Optionally allow or disallow scaling.</param>
+        /// <param name="weight">An optional new weight for the ShapeMatchingGoal.</param>
+        /// <returns name="ShapeMatchingGoal"></returns>
         [NodeCategory("Actions")]
         public static DynaShape.Goals.ShapeMatchingGoal Change(
-            DynaShape.Goals.ShapeMatchingGoal goal,
+            DynaShape.Goals.ShapeMatchingGoal shapeMatchingGoal,
             [DefaultArgument("null")] List<Point> targetShapePoints,
             [DefaultArgument("false")] bool? allowScaling,
             [DefaultArgument("-1.0")] float weight)
         {
-            if (targetShapePoints != null) goal.SetTargetShapePoints(targetShapePoints.ToTriples());
-            if (weight >= 0.0) goal.Weight = weight;
-            if (allowScaling.HasValue) goal.AllowScaling = allowScaling.Value;
-            return goal;
+            if (targetShapePoints != null) shapeMatchingGoal.SetTargetShapePoints(targetShapePoints.ToTriples());
+            if (weight >= 0.0) shapeMatchingGoal.Weight = weight;
+            if (allowScaling.HasValue) shapeMatchingGoal.AllowScaling = allowScaling.Value;
+            return shapeMatchingGoal;
         }
     }
 }
